95871ff8b85740eb216526296d013b6e63c8200a
Timchenko <tbd2504@gmail.com>
2022-06-11 08:57:07 +0300

    second commit

--- task_2.py
+++ task_2.py
@@ -1,8 +1,19 @@
+WORKING = True
+
 integer_part = ''
 fractional_part = ''
 number = ''
 
-number = input('Введіть дрібне число: ')
+
+while WORKING:
+    number = input('Введіть дрібне число: ')
+    try:
+        if float(number) and not number.isdigit():
+            if number[-1] == '.':
+                number += '0'
+            WORKING = False
+    except ValueError:
+        pass
 
 integer_part, fractional_part = number.split('.')
 print(f'Ціла частина числа: {integer_part}\n'
