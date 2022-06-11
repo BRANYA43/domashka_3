99ae7e8fa2cba699160d521d16f17e2cbed7afd5
Timchenko <tbd2504@gmail.com>
2022-06-11 09:50:15 +0300

    second commit

--- task_5.py
+++ task_5.py
@@ -1,12 +1,40 @@
 from random import randint
 
+count = 3
+number = 0
 
-count = 0
-MAX_COUNT = 3
 guess_number = randint(1, 10)
-number = int(input('Введіть число: '))
+print(guess_number)
 
-if guess_number == number and count != MAX_COUNT:
-    print('Вы вгадали')
-else:
-    print('Вы не вгадали')
+
+def get_input_number(message: str):
+    ret = ''
+    while not ret.isdigit():
+        ret = input(message)
+
+    return int(ret)
+
+
+while count > 0:
+    number = get_input_number('Введіть число від 1 до 10: ')
+    if 0 < number < 11:
+
+        if guess_number == number and count != 0:
+            print(f'Вітаємо, Ви вгадали!')
+            break
+        else:
+            print('Ви не вгадали.')
+            print('*' * 20)
+            count -= 1
+
+        if count == 0:
+            print('У вас не залишилось спроб.')
+        elif count == 1:
+            print(f'Залишлось {count} спроба.')
+        elif 2 <= count <= 4:
+            print(f'Залишилось {count} спроби.')
+        elif count >= 5:
+            print(f'Залишилось {count} спроб.')
+    else:
+        print('Ви ввели число вне діпазону.')
+        print('*' * 20)
