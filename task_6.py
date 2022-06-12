1de9d36566ffd3be184c80d8851e957b2a71932c
Timchenko <tbd2504@gmail.com>
2022-06-12 11:05:03 +0300

    third commit

--- task_6.py
+++ task_6.py
@@ -1,8 +1,8 @@
 X_AXIS = tuple(range(1, 9))
 Y_AXIS = tuple(chr(elem) for elem in range(ord('A'), ord('A') + 8))
-DICT_DIRECTION  = {'up': '1', 'down': '2', 'left': '3', 'right': '4'}
+DICT_DIRECTION = {'up': '1', 'down': '2', 'left': '3', 'right': '4'}
 
-knight = {'x': X_AXIS[0], 'y': Y_AXIS[0]}
+knight = {'x': X_AXIS[4], 'y': Y_AXIS[0]}
 save_knight = knight.copy()
 
 all_possible_move = []
@@ -41,9 +41,17 @@ for key in DICT_DIRECTION:
     direction_1 = DICT_DIRECTION[key]
     two_next_direction = get_two_next_direction(direction_1)
     for direction_2 in two_next_direction:
-        set_cord_move(direction_1, knight, distance_move=2)
-        set_cord_move(direction_2, knight, distance_move=1)
-        all_possible_move.append((knight['x'], knight['y']))
+        try:
+            set_cord_move(direction_1, knight, distance_move=2)
+            set_cord_move(direction_2, knight, distance_move=1)
+            if X_AXIS.index(save_knight['x']) - 3 < X_AXIS.index(knight['x']) < X_AXIS.index(save_knight['x']) + 3 \
+                and Y_AXIS.index(save_knight['y']) - 3 < Y_AXIS.index(knight['y']) < Y_AXIS.index(save_knight['y']) + 3:
+
+                all_possible_move.append((knight['x'], knight['y']))
+        except IndexError:
+            pass
+
+
         knight = save_knight.copy()
 print(all_possible_move)
 print(save_knight)
