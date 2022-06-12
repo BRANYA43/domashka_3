aaac8bf6ba7728c4ee76fcb6fea19cbb02a523f0
Timchenko <tbd2504@gmail.com>
2022-06-13 00:54:11 +0300

    third commit

--- task_2.py
+++ task_2.py
@@ -8,12 +8,12 @@ number = ''
 while WORKING:
     number = input('Введіть дрібне число: ')
     try:
-        if float(number) and not number.isdigit():
+        if float(number) and number[-1] == '.':
             if number[-1] == '.':
                 number += '0'
             WORKING = False
     except ValueError:
-        pass
+        print(f'Ви ввели помилкові данні.')
 
 integer_part, fractional_part = number.split('.')
 print(f'Ціла частина числа: {integer_part}\n'
